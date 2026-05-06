8738f892028314f4337c84e71f48d777a82b9520
Shesh <shivaay9670@gmail.com>
2026-05-06 19:55:12 +0530

    Separate backend into pure

--- backend/server.js
+++ backend/server.js
@@ -20,17 +20,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 
-// Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.use((req, res) =>
-    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running...');
-  });
-}
+app.get('/', (req, res) => {
+  res.send('Task Manager API is running securely...');
+});
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/ethara-task-manager';
